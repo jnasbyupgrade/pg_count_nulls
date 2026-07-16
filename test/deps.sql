2722456de6dfac8b18c2fcbba77e00d33e051d72
jnasbyupgrade <jnasby@upgrade.com>
2026-07-16 13:26:15 -0500

    Update .gitignore and test/deps.sql from pgxntool template

--- test/deps.sql
+++ test/deps.sql
@@ -1,8 +1,7 @@
--- Note: pgTap is loaded by setup.sql
-
--- Add any test dependency statements here
 -- IF NOT EXISTS will emit NOTICEs, which is annoying
 SET client_min_messages = WARNING;
+
+-- Add any test dependency statements here
 CREATE SCHEMA IF NOT EXISTS :schema;
 SET search_path = :schema;
 SET client_min_messages = NOTICE;
